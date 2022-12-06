e873d1b463f82937ba843db8b8611acd3d6ea79e
Pim van Hespen <pimvanhespen@gmail.com>
2022-12-06 09:14:27 +0100

    add alternative solve + benchmarks

--- days/06/main.go
+++ days/06/main.go
@@ -49,3 +49,39 @@ func allDifferent(bts []byte) bool {
 	}
 	return true
 }
+
+// -- alternatives --
+
+func solve_map(bts []byte, size int) int {
+	var m = make(map[byte]int)
+	var c byte
+	for i := 0; i < len(bts); i++ {
+		if i > size-1 {
+
+			c = bts[i-size]
+
+			n, ok := m[c]
+			if !ok {
+				panic("missing value in map")
+			}
+
+			if n == 1 {
+				delete(m, c)
+			} else {
+				m[c]--
+			}
+		}
+
+		c = bts[i]
+		if n, ok := m[c]; ok {
+			m[c] = n + 1
+		} else {
+			m[c] = 1
+		}
+
+		if len(m) == size {
+			return i + 1
+		}
+	}
+	return -1
+}
--- days/06/main_test.go
+++ days/06/main_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
 
 func Test_solve1(t *testing.T) {
 	type args struct {
@@ -43,6 +48,96 @@ func Test_solve1(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := solve1(tt.args.bts); got != tt.want {
+				t.Errorf("solve1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_solve_map(t *testing.T) {
+	type args struct {
+		bts []byte
+		len int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "example1",
+			args: args{
+				bts: []byte(`bvwbjplbgvbhsrlpgdmjqwftvncz`),
+				len: 4,
+			},
+			want: 5,
+		},
+		{
+			name: "example1",
+			args: args{
+				bts: []byte(`nppdvjthqldpwncqszvftbrmjlhg`),
+				len: 4,
+			},
+			want: 6,
+		},
+		{
+			name: "example1",
+			args: args{
+				bts: []byte(`nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`),
+				len: 4,
+			},
+			want: 10,
+		},
+		{
+			name: "example1",
+			args: args{
+				bts: []byte(`zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`),
+				len: 4,
+			},
+			want: 11,
+		},
+
+		{
+			name: "example2",
+			args: args{
+				bts: []byte(`mjqjpqmgbljsphdztnvjfqwrcgsmlb`),
+				len: 14,
+			},
+			want: 19,
+		}, {
+			name: "example2",
+			args: args{
+				bts: []byte(`bvwbjplbgvbhsrlpgdmjqwftvncz`),
+				len: 14,
+			},
+			want: 23,
+		}, {
+			name: "example2",
+			args: args{
+				bts: []byte(`nppdvjthqldpwncqszvftbrmjlhg`),
+				len: 14,
+			},
+			want: 23,
+		}, {
+			name: "example2",
+			args: args{
+				bts: []byte(`nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`),
+				len: 14,
+			},
+			want: 29,
+		},
+		{
+			name: "example2",
+			args: args{
+				bts: []byte(`zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`),
+				len: 14,
+			},
+			want: 26,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solve_map(tt.args.bts, tt.args.len); got != tt.want {
 				t.Errorf("solve1() = %v, want %v", got, tt.want)
 			}
 		})
@@ -98,4 +193,68 @@ func Test_solve2(t *testing.T) {
 			}
 		})
 	}
+}
+
+func BenchmarkSolve1(b *testing.B) {
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f.Close()
+
+	input, err := io.ReadAll(f)
+	if err != nil {
+		b.Error(err)
+	}
+
+	var total int
+
+	b.Run("allDifferent", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			total += solve1(input)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			total += solve_map(input, 4)
+		}
+	})
+
+	fmt.Println(total)
+}
+
+func BenchmarkSolve2(b *testing.B) {
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f.Close()
+
+	input, err := io.ReadAll(f)
+	if err != nil {
+		b.Error(err)
+	}
+
+	var total int
+
+	b.Run("allDifferent", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			total += solve2(input)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			total += solve_map(input, 14)
+		}
+	})
+
+	fmt.Println(total)
 }

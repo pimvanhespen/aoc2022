e5570f658f035556b9807fd68840bd9848859061
Pim van Hespen <pimvanhespen@gmail.com>
2022-12-17 12:58:49 +0100

    day 17.1 write tetris

--- days/17/main.go
+++ days/17/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/pimvanhespen/aoc2022/pkg/aoc"
 	"io"
+	"strings"
 )
 
 func main() {
@@ -17,22 +19,360 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Part 1:", solve1(input))
+	fmt.Println("Part 1:", solve1(input, 2022))
 	fmt.Println("Part 2:", solve2(input))
+
 }
 
-type Input struct {
-	// todo
+type Jets struct {
+	data   []byte
+	offset int
 }
 
-func parse(reader io.Reader) (Input, error) {
-	panic("not implemented")
+func (j *Jets) Next() {
+	j.offset++
 }
 
-func solve1(input Input) int {
-	panic("not implemented")
+func (j *Jets) Move() Vec2 {
+	if j.data[j.offset%len(j.data)] == '<' {
+		return Vec2{X: -1}
+	}
+	return Vec2{X: 1}
 }
 
-func solve2(input Input) int {
-	panic("not implemented")
+func parse(reader io.Reader) (Jets, error) {
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		return Jets{}, err
+	}
+
+	return Jets{data: bytes.Split(b, []byte{'\n'})[0]}, nil
 }
+
+var (
+	HLineString = `####`
+	PlusString  = `.#.
+###
+.#.`
+	CornersString = `..#
+..#
+###`
+
+	VLineString = `#
+#
+#
+#`
+
+	SquareString = `##
+##`
+)
+
+var (
+	VerticalLine   = NewShape(VLineString)
+	HorizontalLine = NewShape(HLineString)
+	Plus           = NewShape(PlusString)
+	Corner         = NewShape(CornersString)
+	Square         = NewShape(SquareString)
+)
+
+var shapes = []Shape{HorizontalLine, Plus, Corner, VerticalLine, Square}
+
+type Field struct {
+	height int
+	width  int
+	blocks []Block
+}
+
+func (f Field) CanMove(shape Shape, pos Vec2) bool {
+	block := Block{
+		Position: pos,
+		Shape:    shape,
+	}
+	if block.MinX() < 0 {
+		return false
+	}
+	if block.MaxX() > f.width-1 {
+		return false
+	}
+
+	if block.MinY() < 0 {
+		return false
+	}
+
+	return !f.HasCollisonWithBlocks(block)
+}
+
+func (f Field) HasCollisonWithBlocks(block Block) bool {
+	for i := len(f.blocks) - 1; i >= 0; i-- {
+		if f.blocks[i].Collides(block) {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *Field) Add(shape Shape, pos Vec2) {
+	block := Block{
+		Position: pos,
+		Shape:    shape,
+	}
+	f.height = max(f.height, 1+block.MaxY())
+	f.blocks = append(f.blocks, block)
+}
+
+func (f Field) Copy() Field {
+	cp := Field{
+		height: f.height,
+		width:  f.width,
+		blocks: make([]Block, len(f.blocks)),
+	}
+
+	for i, b := range f.blocks {
+		cp.blocks[i] = b
+	}
+
+	return cp
+}
+
+func (f Field) String() string {
+
+	allPts := make([]Vec2, 0, len(f.blocks)*5)
+
+	for _, b := range f.blocks {
+		allPts = append(allPts, b.Point()...)
+	}
+
+	var maxY int
+	for _, p := range allPts {
+		maxY = max(maxY, p.Y)
+	}
+
+	bts := make([][]byte, maxY+1)
+	for i := range bts {
+		bts[i] = bytes.Repeat([]byte{'.'}, f.width)
+	}
+
+	for _, block := range f.blocks {
+		pts := block.Point()
+		for _, p := range pts {
+			y := maxY - p.Y
+			bts[y][p.X] = '@'
+		}
+	}
+
+	var sb strings.Builder
+	for _, l := range bts {
+		sb.WriteByte('|')
+		sb.Write(l)
+		sb.WriteByte('|')
+		sb.WriteByte('\n')
+	}
+
+	sb.WriteRune('+')
+	sb.Write(bytes.Repeat([]byte{'-'}, f.width))
+	sb.WriteRune('+')
+
+	return sb.String()
+}
+
+type Bounds struct {
+	minX, maxX, minY, maxY int
+}
+
+type Block struct {
+	Position Vec2
+	Shape    Shape
+}
+
+func (b Block) MinY() int {
+	return b.Position.Y
+}
+
+func (b Block) MaxY() int {
+	return b.Position.Y + b.Shape.height - 1
+}
+
+func (b Block) MinX() int {
+	return b.Position.X
+}
+
+func (b Block) MaxX() int {
+	return b.Position.X + b.Shape.width - 1
+}
+
+func (b Block) Bounds() Bounds {
+	return Bounds{
+		minX: b.MinX(),
+		maxX: b.MaxX(),
+		minY: b.MinY(),
+		maxY: b.MaxY(),
+	}
+}
+
+func (b Block) Collides(other Block) bool {
+	// check X overlap
+	if other.MaxX() < b.MinX() || other.MinX() > b.MaxX() ||
+		other.MaxY() < b.MinY() || other.MinY() > b.MaxY() {
+		return false
+	}
+
+	ps1, ps2 := b.Point(), other.Point()
+	for _, p1 := range ps1 {
+		for _, p2 := range ps2 {
+			if p1.X == p2.X && p1.Y == p2.Y {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (b Block) Point() []Vec2 {
+	points := make([]Vec2, 0, len(b.Shape.fields))
+	for _, p := range b.Shape.fields {
+		points = append(points, b.Position.Add(p.X, p.Y))
+	}
+	return points
+}
+
+type Vec2 struct {
+	X, Y int
+}
+
+func (v Vec2) Add(x, y int) Vec2 {
+	return Vec2{
+		X: v.X + x,
+		Y: v.Y + y,
+	}
+}
+
+type Shape struct {
+	height int
+	width  int
+	fields []Vec2
+}
+
+func NewShape(s string) Shape {
+	numFields := strings.Count(s, "#")
+	lines := strings.Split(s, "\n")
+	width := len(lines[0])
+	height := len(lines)
+
+	fields := make([]Vec2, 0, numFields)
+
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '#' {
+				fields = append(fields, Vec2{X: x, Y: height - 1 - y})
+			}
+		}
+	}
+
+	return Shape{
+		width:  width,
+		height: height,
+		fields: fields,
+	}
+}
+
+func (s Shape) Width() int {
+	return s.width
+}
+
+func (s Shape) Height() int {
+	return s.height
+}
+
+type Mover struct {
+	isHorizontal bool
+	stream       []byte
+	offset       int
+}
+
+func NewMover(stream Jets) *Mover {
+	return &Mover{
+		isHorizontal: true,
+		stream:       stream.data,
+	}
+}
+
+func (m *Mover) Reset() {
+	m.isHorizontal = false
+}
+
+func (m *Mover) Next() (Vec2, bool) {
+	m.isHorizontal = !m.isHorizontal
+
+	if m.isHorizontal {
+		c := m.stream[m.offset%len(m.stream)]
+		m.offset++
+		if c == '<' {
+			return Vec2{X: -1}, true
+		}
+
+		return Vec2{X: 1}, true
+	}
+
+	return Vec2{Y: -1}, false
+}
+
+func solve1(input Jets, blocks int) int {
+	field := Field{
+		width:  7,
+		blocks: make([]Block, 0, blocks),
+	}
+
+	defaultOffset := Vec2{
+		X: 2,
+		Y: 3,
+	}
+
+	mover := NewMover(input)
+
+	for i := 0; i < blocks; i++ {
+		shape := shapes[i%len(shapes)]
+		position := defaultOffset.Add(0, field.height)
+		mover.Reset()
+
+		for {
+			move, isHorizontal := mover.Next()
+			next := position.Add(move.X, move.Y)
+
+			if field.CanMove(shape, next) {
+				position = next
+			} else if !isHorizontal {
+				break
+			}
+
+			//c := field.Copy()
+			//c.Add(shape, position)
+			//fmt.Println(c)
+		}
+
+		field.Add(shape, position)
+		//fmt.Println(field)
+	}
+
+	return field.height
+}
+
+func solve2(input Jets) int {
+	return -1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+var blocks = 1_000_000_000_000
--- days/17/main_test.go
+++ days/17/main_test.go
@@ -1,1 +1,121 @@
 package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const testInput = `>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>`
+
+func TestSolve1(t *testing.T) {
+	input, err := parse(strings.NewReader(testInput))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := solve1(input, 2022)
+	want := 3068
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestBlock_Point(t *testing.T) {
+	type fields struct {
+		Position Vec2
+		Shape    Shape
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []Vec2
+	}{
+		{
+			name: "test",
+			fields: fields{
+				Position: Vec2{X: 1, Y: 2},
+				Shape: Shape{
+					fields: []Vec2{
+						{X: 0, Y: 0},
+						{X: 1, Y: 0},
+						{X: 0, Y: 1},
+					},
+				},
+			},
+			want: []Vec2{
+				{X: 1, Y: 2},
+				{X: 2, Y: 2},
+				{X: 1, Y: 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Block{
+				Position: tt.fields.Position,
+				Shape:    tt.fields.Shape,
+			}
+			if got := b.Point(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Point() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestField_String(t *testing.T) {
+	const want = `|....#..|
+|...###.|
+|....#..|
+|...####|
++-------+`
+	field := Field{width: 7}
+	field.Add(HorizontalLine, Vec2{3, 0})
+	field.Add(Plus, Vec2{3, 1})
+
+	s := field.String()
+	if s != want {
+		var sb strings.Builder
+		sb.WriteString("got:\t\twant:\n")
+		pw, pg := strings.Split(want, "\n"), strings.Split(s, "\n")
+		for i := 0; i < max(len(pw), len(pg)); i++ {
+			if i < len(pg) {
+				sb.WriteString(pg[i])
+			} else {
+				sb.WriteString(strings.Repeat(" ", len(pw[i])))
+			}
+			sb.WriteByte('\t')
+			if i < len(pw) {
+				sb.WriteString(pw[i])
+			}
+			sb.WriteByte('\n')
+		}
+		t.Errorf(sb.String())
+	}
+}
+
+func TestBounds(t *testing.T) {
+	type args struct {
+		block Block
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want Bounds
+	}{
+		{
+			name: "test",
+			args: args{
+				block: Block{Position: Vec2{X: 0, Y: 0}, Shape: Plus},
+			},
+			want: Bounds{minX: 0, maxX: 2, minY: 0, maxY: 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.args.block.Bounds(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Bounds() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
